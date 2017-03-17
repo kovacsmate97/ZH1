895f68f0758e9194e6784a7692fd309c89a58049
kovacsmate97 <kovimatyi8@freemail.hu>
2017-03-17 10:04:01 +0100

    Verseny

---
+++ ZH1/Verseny.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZH1
+{
+    public class Verseny
+    {
+        private int Atlagsebesseg;
+        private int MegtettTav;
+        private int Korok;
+        private int Labankenti_sebesseg;
+        private int VersenyzoID;
+
+        public void Tajekoztat()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
