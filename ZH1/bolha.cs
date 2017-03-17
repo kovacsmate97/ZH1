845ce0a77bca4556e42ed908c4d4a4b62d2e6cf6
kovacsmate97 <kovimatyi8@freemail.hu>
2017-03-17 10:13:29 +0100

    Bolha

--- ZH1/bolha.cs
+++ ZH1/bolha.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text;
 
 namespace ZH1
+{    
+    public class Bolha : Barna_a_medve
 {
-    public class bolha
-    {
-    }
-
-    public class Bolha
-    {
         public void Kapaszkodik()
+        {
+            
+        }
+
+        public void Halad()
         {
             throw new System.NotImplementedException();
         }
