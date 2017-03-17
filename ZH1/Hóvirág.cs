fdebede727d4e99246774faa0661108dea6856ee
kovacsmate97 <kovimatyi8@freemail.hu>
2017-03-17 10:04:33 +0100

    Hóvirág

---
+++ ZH1/Hóvirág.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZH1
+{
+    public class Hóvirág
+    {
+        public void FoldbeGyokerezik()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
